f071839fd9db07575e58f91f6b84f018a66b51c5
raazadarsh <raj.ada@northeastern.edu>
2025-01-21 01:16:53 -0500

    fix: added lazy singleton in the java object creation process

--- src/main/java/edu/neu/csye7374/StockMarket.java
+++ src/main/java/edu/neu/csye7374/StockMarket.java
@@ -1,6 +1,26 @@
 package edu.neu.csye7374;
 
 public class StockMarket {
+    // Single instance of the StockMarket
+    private static StockMarket instance;
+
+    // Private constructor to prevent instantiation
+    private StockMarket() {
+        // Initialization logic, if any
+    }
+
+    // Public static method to get the singleton instance
+    public static StockMarket getInstance() {
+        if (instance == null) {
+            // Synchronized block to ensure thread safety
+            synchronized (StockMarket.class) {
+                if (instance == null) {
+                    instance = new StockMarket();
+                }
+            }
+        }
+        return instance;
+    }
     public static void demo() {
         Stock stock1 = new StockSubclass1("TESLA", 422.07, "Tesla Common Stock");
         Stock stock2 = new StockSubclass2("APPLE", 170.20, "Apple Inc. Common Stock");
